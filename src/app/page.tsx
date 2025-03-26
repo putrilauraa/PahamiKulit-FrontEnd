7b1e58516ca56949b6796429a255ab3261a65329
putrilauraa <rahma.marvialdi@binus.ac.id>
2025-03-26 21:20:12 +0700

    test commit 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,6 +22,7 @@ export default function Home() {
           </li>
           <li>Save and see your changes instantly.</li>
           <li>YAHAHAHA HAAYUUUKKKKK OKE GASS OKE GASS.</li>
+          <li>HAYUKK KEMANA KOCAKKKK! WOI LAH.</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
