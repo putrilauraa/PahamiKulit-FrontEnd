547b96917ca13708a046c13b7cc0efe70c14360c
FatinRoz <fatinrozsidhy@gmail.com>
2025-05-04 20:46:57 +0700

    feat(skincare): skincare dashboard

--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -1,10 +1,82 @@
 import Navbar from "@/components/Navbar"
+import { Footer } from '@/components/Footer';
+import { Montserrat } from 'next/font/google';
+
+const montserrat = Montserrat({ weight: '500', subsets: ['latin'] });
+
 
 export default function AboutUs() {
     return (
         <>
         <Navbar/>
             <h1>AboutUs Page</h1>
-        </>
+
+            <div
+                className={`${montserrat.className}`}
+                style={{
+                    color: '#000000',
+                }}>
+
+            
+<div className="grid grid-cols-1 md:grid-cols-2 grid-rows-4 md:grid-rows-2 gap-2 py-14 px-32 h-1/5">
+            {/* About Us */}
+            <div className="flex items-center">
+                    <div className="border-pink-200 bg-pink-200 border-2 px-10 py-10 rounded-4xl">
+                {/* Ilustrasi 1 */}
+                <div className="p-4 flex items-center space-x-4">
+                    <img
+                        className="w-100 h-auto mt-4"
+                        src="/Pink-11.png"
+                        alt="Mascot"
+                        >
+                    </img>
+                </div>
+                
+                {/* Tile */}
+                <div className="font-semibold mb-3 text-2xl">
+                    About Us
+                </div>
+
+                {/* Description */}
+                <p>
+                BasicDulu adalah website edukasi skincare 
+                untuk membantu masyarakat yang masih awam 
+                dengan skincare dalam memahami dasar-dasar 
+                perawatan kulit. Kami hadir dengan memfokuskan 
+                pengemasan informasi secara simpel, praktis, 
+                dan mudah dipahami agar dapat dengan mudah 
+                dimengerti bagi pemula.
+                </p>
+                
+
+                    
+
+
+                <div>
+                    <p>Hello sucker</p>
+                </div>
+                    
+                </div>
+            </div>
+
+            {/* Visi */}
+            <div className="Card">
+
+            </div>
+
+            </div>
+            {/* Misi */}
+
+            {/* Footer Introduction */}
+
+
+            </div>
+
+            <div className="mt-6">
+                <Footer />
+            </div>
+
+        </>   
+        
     );
 }
--- src/app/skincare/page.tsx
+++ src/app/skincare/page.tsx
@@ -1,10 +1,136 @@
 import Navbar from '@/components/Navbar';
+import { Footer } from '@/components/Footer';
+import Link from 'next/link';
+import { Montserrat } from 'next/font/google';
+
+const montserrat = Montserrat({ weight: '500', subsets: ['latin'] });
 
 export default function SkinCare() {
     return (
         <>
             <Navbar />
-            <h1>SkinCare Page</h1>
+
+
+            <div className={'${montserrat.className}'}
+                style={{
+                    color: '#000000',
+                }}
+            >
+
+                <div className="grid grid-cols-1 md:grid-cols-2 grid-rows-4 md:grid-rows-2 gap-2 py-14 px-32 h-1/5">
+                    {/* Column */}
+                    <div className="flex items-center">
+
+                        <div className="border-pink-200 border-2 px-10 py-10 rounded-4xl hover:text-black hover:bg-pink-200">
+                            {/* Title */}
+                            <div className="font-semibold mb-3 text-2xl">
+                                Panduan Basic Skincare
+                            </div>
+
+                            {/* Description */}
+                            <p>
+                                Pelajari langkah-langkah dasar perawatan kulit,
+                                mulai dari cara membersihkan, melembapkan,
+                                hingga melindungi kulit.
+                                Cocok untuk pemula yang ingin mulai
+                                merawat kulit dengan cara yang tepat.
+                            </p>
+
+                            <div className="flex justify-end">
+                                <button className="border-pink-200 border-2 px-4 py-1.5 mt-4 rounded-4xl hover:text-white hover:bg-pink-300">
+                                    Telusuri →
+                                </button>
+                            </div>
+
+                        </div>
+                    </div>
+
+                    <div className="flex items-center">
+                        <div className="border-blue-200 border-2 px-10 py-10 rounded-4xl hover:text-black hover:bg-blue-200">
+                            {/* Title */}
+                            <div className="font-semibold mb-3 text-2xl">
+                                Mengenal Bahan Aktif
+                            </div>
+
+                            {/* Description */}
+                            <p>
+                                Memadukan kandungan skincare tidak boleh asal loh!
+                                Ada beberapa bahan aktif skincare yang tidak boleh
+                                digunakan secara bersamaan, apa aja ya kira-kira?
+                                Yuk cari tahu disini!
+                            </p>
+
+                            <div className="flex justify-end">
+                                <button className="border-blue-200 border-2 px-4 py-1.5 mt-4 rounded-4xl hover:text-white hover:bg-blue-300">
+                                    Telusuri →
+                                </button>
+                            </div>
+
+                        </div>
+                    </div>
+
+                    <div className="flex items-center">
+                        <div className="border-blue-200 border-2 px-10 py-10 rounded-4xl hover:text-black hover:bg-blue-200">
+                            {/* Title */}
+                            <div className="font-semibold mb-3 text-2xl">
+                                Rekomendasi Skincare
+                            </div>
+
+                            {/* Description */}
+                            <p>
+                                Dapatkan rekomendasi skincare terbaik untuk jenis
+                                kulitmu. Mulai dari yang berminyak, kering, sensitif,
+                                sampai berjerawat. Nggak perlu repot tebak-tebakan
+                                lagi, semua info disajikan dengan jelas, praktis,
+                                dan gampang dipahami.
+                            </p>
+
+                            <div className="flex justify-end">
+                                <button className="border-blue-200 border-2 px-4 py-1.5 mt-4 rounded-4xl hover:text-white hover:bg-blue-300">
+                                    Telusuri →
+                                </button>
+                            </div>
+
+                        </div>
+                    </div>
+
+                    <div className="flex items-center">
+                        <div className="border-pink-200 border-2 px-10 py-10 rounded-4xl hover:text-black hover:bg-pink-200">
+                            {/* Title */}
+                            <div className="font-semibold mb-3 text-2xl">
+                                Rekomendasi Skincare
+                            </div>
+
+                            {/* Description */}
+                            <p>
+                                Dapatkan rekomendasi skincare terbaik untuk jenis
+                                kulitmu. Mulai dari yang berminyak, kering, sensitif,
+                                sampai berjerawat. Nggak perlu repot tebak-tebakan
+                                lagi, semua info disajikan dengan jelas, praktis,
+                                dan gampang dipahami.
+                            </p>
+
+                            <div className="flex justify-end">
+                                <button className="border-pink-200 border-2 px-4 py-1.5 mt-4 rounded-4xl hover:text-white hover:bg-pink-300">
+                                    Telusuri →
+                                </button>
+                            </div>
+
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+            <div className="mt-12">
+                <Footer />
+            </div>
+
         </>
+
+
     );
 }
+
+
+
