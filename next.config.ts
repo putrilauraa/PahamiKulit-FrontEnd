f401842952f9677d482ca4d449f87f45cc1ecd1d
putrilauraa <rahma.marvialdi@binus.ac.id>
2025-06-27 15:47:39 +0700

    fix(config): change host config to fly io

--- next.config.ts
+++ next.config.ts
@@ -3,10 +3,16 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
-      {
+      /*{
         protocol: 'http',
         hostname: 'localhost',
         port: '8080',
+        pathname: '/static/uploads/**',
+      },*/
+      {
+        protocol: 'http',
+        hostname: 'backend-solitary-sea-3762.fly.dev',
+        port: '',
         pathname: '/static/uploads/**',
       },
     ],
